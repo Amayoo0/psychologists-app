541ff822120c0304a7149e3d5bb1106ea59fefa6
amayoodoodonto <amayov4c@gmail.com>
2025-03-30 14:08:34 +0200

    fixes bug in VideoCall user name

--- src/components/videocall/StreamVideoProvider.tsx
+++ src/components/videocall/StreamVideoProvider.tsx
@@ -12,23 +12,26 @@ const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
     const [videoClient, setVideoClient] = useState<StreamVideoClient>()
     const { user } = useUser();
-    
     useEffect(() => {
-        // if (!isLoaded || !user) return;
         if (!apiKey) throw new Error('Stream API key missing');
-
+        
+        let userName = 'Invitado';
+        if (user) {
+            userName = user?.emailAddresses[0].emailAddress.toString().split('@')[0];
+        }
+        
         const client = new StreamVideoClient({
             apiKey,
             user: {
                 id: user?.id || "Invitado",
-                name: user?.emailAddresses ? String(user?.emailAddresses) : "Invitado",
+                name: userName,
                 image: user?.imageUrl
             },
             tokenProvider,
         })
 
         setVideoClient(client)
-    }, []);
+    }, [user]);
 
     if (!videoClient) return null
 
